f728d90b693d2636acbdbf243a3aeae00c633852
musmanqazi <usmanqazi8@gmail.com>
2024-08-02 20:17:17 -0700

    added my first Material UI component

--- app/page.js
+++ app/page.js
@@ -1,5 +1,14 @@
+'use client'
 import Image from "next/image";
+import { useState, useEffect } from 'react';
+import { firestore } from '@/firebase';
+import { Box, Typography } from "@mui/material";
 
 export default function Home() {
-  return <div>Hello World</div>
+  return (
+  // Box is the most basic Material UI Component
+  <Box>
+    <Typography variant="h1">Pantry Tracker</Typography>
+  </Box>
+  )
 }
