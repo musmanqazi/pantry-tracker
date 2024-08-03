a9262112b64b469f10cd61e36b6d6b4696d74509
musmanqazi <usmanqazi8@gmail.com>
2024-08-02 23:35:29 -0700

    basic app complete

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { useState, useEffect } from 'react';
 import { firestore } from '@/firebase';
 import { Box, Button, Modal, Stack, TextField, Typography } from "@mui/material";
-import { collection, getDocs, query } from "firebase/firestore";
+import { collection, getDocs, query, doc, getDoc, setDoc, deleteDoc } from "firebase/firestore";
 
 export default function Home() {
   const [pantry, setPantry] = useState([]) // Will store the inventory here
@@ -24,8 +24,8 @@ export default function Home() {
     console.log(pantryList)
   }
 
-  const addItem = async () => { // Helper function 2
-    const docRef = doc(collection(firestore, 'inventory'), item)
+  const addItem = async (item) => { // Helper function 2
+    const docRef = doc(collection(firestore, 'pantry'), item)
     const docSnap = await getDoc(docRef)
 
     if(docSnap.exists()) {
@@ -37,8 +37,8 @@ export default function Home() {
     await updatePantry()
   }
 
-  const removeItem = async () => { // Helper function 3
-    const docRef = doc(collection(firestore, 'inventory'), item)
+  const removeItem = async (item) => { // Helper function 3
+    const docRef = doc(collection(firestore, 'pantry'), item)
     const docSnap = await getDoc(docRef)
 
     if(docSnap.exists()) {
@@ -65,6 +65,7 @@ export default function Home() {
     width="100vw"
     height="100vh"
     display="flex"
+    flexDirection="column"
     justifyContent="center"
     alignItems="center"
     gap={2}
@@ -86,8 +87,16 @@ export default function Home() {
           transform: "translate(-50%, -50%)"
         }}
       >
-        <Typography variant="h6">Add Item</Typography>
-        <Stack width="100%" direction="row" spacing={2}>
+        <Typography
+          variant="h6"
+        >
+          Add Item
+        </Typography>
+        <Stack
+          width="100%"
+          direction="row"
+          spacing={2}
+        >
           <TextField
             variant="outlined"
             fullWidth
@@ -109,7 +118,83 @@ export default function Home() {
         </Stack>
       </Box>
     </Modal>
-    <Typography variant="h1">Pantry Tracker</Typography>
+    {/* <Typography variant="h1">Pantry Tracker</Typography> */}
+    <Button
+      variant="contained"
+      onClick={() => {
+        handleOpen()
+      }}
+    >
+      Add New Item
+    </Button>
+    <Box border="1px solid #333">
+      <Box
+        width="800px"
+        height="100px"
+        bgcolor="#ADD8E6"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Typography
+          variant="h2"
+          color="#333"
+        >
+          Pantry Items
+        </Typography>
+      </Box>
+      <Stack
+        width="800px"
+        height="300px"
+        overflow="auto"
+      >
+        {
+          pantry.map(({name, quantity}) => (
+            <Box key={name} 
+              width="100%" 
+              minHeight="150px" 
+              display="flex" 
+              alignItems="center" 
+              justifyContent="space-between" 
+              bgcolor="#f0f0f0"
+              padding={5}
+              >
+                <Typography 
+                  variant="h3"
+                  color="#333"
+                  textAlign="center"
+                >
+                  {name.charAt(0).toUpperCase() + name.slice(1)}
+                </Typography>
+                <Typography 
+                  variant="h3"
+                  color="#333"
+                  textAlign="center"
+                >
+                  {quantity}
+                </Typography>
+                <Stack direction="row" spacing={2}>
+                  <Button
+                  variant="contained"
+                  onClick={() => {
+                    addItem(name)
+                  }}
+                  >
+                    Add
+                  </Button>
+                  <Button
+                  variant="contained"
+                  onClick={() => {
+                    removeItem(name)
+                  }}
+                  >
+                    Remove
+                  </Button>
+                </Stack>
+              </Box>
+          ))}
+      </Stack>
+    </Box>
   </Box>
   )
 }
